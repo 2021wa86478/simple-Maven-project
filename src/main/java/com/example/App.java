93d1de2ccfc6a20ef6bb46bbe9be0dcb5d528d9d
2021wa86478 <2021wa86478@wilp.bits-pilani.ac.in>
2025-07-13 11:59:53 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins CI/CD Pipeline");
+        System.out.println("Hello pratik, Jenkins CI/CD Pipeline");
     }
 }
